5cfc5b6f42d371b0c7185e469152e5fbb70a692b
Kel Ming <kkgm013@gmail.com>
2020-02-22 12:24:05 +0000

    Multiple Updates
    - Gameplay Started
    - Updated MenuController Script
    - Implemented Dont Destroy Script
    - Audio Issue: Can't alternate & Destroy between audios between Main Menu and Gameplay

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,38 @@ public class MenuController : MonoBehaviour
     {
         
     }
+
+    /* Head back to the Main Menu */
+    public void ButtonHandlerMenu(){
+        SceneManager.LoadSceneAsync(0);
+    }
     
+    /* Head to Game based on Save State*/
+    public void ButtonHandlerContinue(){
+
+    }
+
+    /*Head to Gamplay Scene */
     public void ButtonHandlerPlay(){
-        // Async Add 
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    /* Head to the Options Scene */
+    public void ButtonHandlerOptions(){
+        // Async Add
 		SceneManager.LoadSceneAsync(1);
 	}
+
+    /* Quit the Game */
+    public void QuitGame(){
+        // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+    }
+
 }
