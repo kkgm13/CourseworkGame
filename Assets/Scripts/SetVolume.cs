9490eb162931157f40804a49b986570ae65ea89c
Kel Ming <kkgm013@gmail.com>
2020-02-22 00:59:58 +0000

    Audio Mixers Now Syncing
    Audio Mixers properly syncing with Sliders. Will require further investigation regarding Scene Transfering and retaining Sound levels

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string mixerName;
+
+    public void SetLevel(float sliderVal){
+        mixer.SetFloat(mixerName,
+        Mathf.Log10(sliderVal) * 20 // Slider Value respresented by the Log base of 10 & multiply by 20
+        );
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
