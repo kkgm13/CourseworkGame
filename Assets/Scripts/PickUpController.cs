6135ae0f6d0e0c34888cc46fed392d55b6f425b5
Kel Ming <kkgm013@gmail.com>
2020-03-03 09:19:40 +0000

    Gameplay Updated
    - Coin Added and Score working now
    - [ISSUE] Coin music not playing due to triggering the Destroy.
    - Environment Updates
    - Minimal Script Removal

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PickUpController : MonoBehaviour
+{
+    public int degrees;
+    public Text ScoreText;
+    private int currentScore = 0;
+    public AudioSource collectSound;
+
+    void Awake(){
+        collectSound = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = "Score: " + currentScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate Coin Around
+        transform.Rotate(0,degrees*Time.deltaTime,0);
+    }
+
+    void OnTriggerEnter(Collider collider){
+        if(collider.tag == "Player"){
+            collectSound.Play();
+            currentScore += 10;
+            ScoreText.text = "Score: " + currentScore;
+            // Deleting without playing sound
+            Destroy(this.gameObject);
+        }
+    }
+}
