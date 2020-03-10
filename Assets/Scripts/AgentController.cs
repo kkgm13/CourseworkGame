8fa27187a7ef06711dad3227824b04baea75b366
Kel Ming <kkgm013@gmail.com>
2020-03-10 20:25:16 +0000

    NavMesh Added
    - Added Navigational Mesh to Gameplay
    - Added AgentController Script which seems to work
    - [ISSUE] Walking Animation stops when climbing
    - [INVESTIGATE] Destroys Coin Object but states it plays the audio for some reason.

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    //Nav Mesh Agent States
+    public enum AgentState {
+		Idle = 0,
+		WalkAround,
+		Chasing
+	}
+    public AgentState state; 
+
+    // All waypoint information
+    public Transform[] waypoints;
+    // Starting WayPoint ID (Custom for each Agent)
+    public int waypointID;
+    // Get the distance to next point
+    public float distanceToStartHeadingToNextWaypoint = 1;
+    // Target Object
+    public Transform target;
+    // Navigational Mesh Agent
+    private NavMeshAgent navMeshAgent;
+    // Access to Animator Controller 
+    private Animator animController;
+    // Hashed Walking Speed ID
+    private int walkSpeedID;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){
+        walkSpeedID = Animator.StringToHash("walkingSpeed");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animController = GetComponent<Animator>();
+
+        // If the list of waypoints is nothing
+        if(waypoints.Length == 0){
+            // Destroy Object
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == AgentState.Idle){
+            Idle();
+        } else if(state == AgentState.WalkAround){
+            WalkAround();
+        }
+    }
+
+    void WalkAround(){
+        navMeshAgent.isStopped = false;
+        navMeshAgent.stoppingDistance = 0;
+
+        animController.SetFloat(walkSpeedID, 1.0f);
+
+        if(navMeshAgent.remainingDistance <= distanceToStartHeadingToNextWaypoint){
+            waypointID = (waypointID+1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[waypointID].position);
+        }
+    }
+
+    void Idle(){
+        navMeshAgent.isStopped = true;
+        animController.SetFloat(walkSpeedID, 0.0f);
+    }
+}
