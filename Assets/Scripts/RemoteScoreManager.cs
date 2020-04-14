61d180b5869f2961a01f9ca966c6db902a0d496a
Kel Ming <kkgm013@gmail.com>
2020-04-14 19:35:14 +0100

    Multiple Updates
    - Updated All Scenes
    - Included new custom menu/option music
    - Started with RemoteScore Manager System
    - Started with Save/Restore State system
    - FIX: Weird 2 audio listeners in FPS

---
+++ Assets/Scripts/RemoteScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using System;
+
+public class ScoreResult {
+    public int Coins;
+    public string code;
+    public string message;
+}
+
+public class RemoteScoreManager : MonoBehaviour
+{
+    public static RemoteScoreManager Instance { get; private set; }
+    
+    void Awake(){
+        if(Instance == null){
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator GetCoinScore(Action<int> onCompleteCallback){
+        string url = "https://api.backendless.com/"+Globals.APP_ID+"/"+Globals.REST_ID+"/data/DataBase";
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        request.SetRequestHeader("application-id", Globals.APP_ID);
+        request.SetRequestHeader("secret-key", Globals.REST_ID);
+        request.SetRequestHeader("application-type", "REST");
+        yield return request.Send();
+
+        if(request.isNetworkError){
+            Debug.Log("Error "+request.responseCode+": "+request.error);
+        } else {
+            ScoreResult scoreData = JsonUtility.FromJson<ScoreResult>(request.downloadHandler.text);
+            if(!string.IsNullOrEmpty(scoreData.code)){
+                Debug.Log("Error: "+scoreData.message+"\n("+scoreData.code+")");
+            } else {
+                onCompleteCallback(scoreData.Coins);
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Unity3D 2018.3.0 (University provides 2018.2.18 / Lax with 2019.3)
 - Utilized due to macOS issue with Overall Audio Utilization.
 
 ## Packages Utilized
--  [Github for Unity](https://assetstore.unity.com/packages/tools/version-control/github-for-unity-118069)
+- [Github for Unity](https://assetstore.unity.com/packages/tools/version-control/github-for-unity-118069)
 - [Unity Standard Assets](https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-for-unity-2017-3-32351)
 - [Nature Starter Kit 2](https://assetstore.unity.com/packages/3d/environments/nature-starter-kit-2-52977)
-- [Space Robot Kyle](https://assetstore.unity.com/packages/3d/characters/robots/space-robot-kyle-4696)
+- 
+
+## External Research
+- [Post-Processing for Underwater - Romi Fauzi](https://www.youtube.com/watch?v=zmDDR9mOVTs)
+- [Collectable Scoring System - Jimmy Vegas](https://www.youtube.com/watch?v=D0lx90n0s-4)
