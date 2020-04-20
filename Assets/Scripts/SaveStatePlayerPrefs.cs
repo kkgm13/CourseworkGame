d26a26e9e7aff86063abcf7d9b0618dc9f409a10
Kel Ming <kkgm013@gmail.com>
2020-04-20 08:27:47 +0100

    Alterations
    - Saving in progress
    - Updated Main Menu & Gameplay
    - Logic Changes with how load/continue works

--- Assets/Scripts/SaveStatePlayerPrefs.cs
+++ Assets/Scripts/SaveStatePlayerPrefs.cs
@@ -8,30 +8,21 @@ public class SaveStatePlayerPrefs : MonoBehaviour
     public GameObject thePlayer;
     public float xPos, yPos, zPos;
     public float xRot, yRot, zRot;
+    public GameObject coinStash;
 
     // Start is called before the first frame update
     void Start(){
-        
         xPos = PlayerPrefs.GetFloat("PlXPos"); 
         yPos = PlayerPrefs.GetFloat("PlYPos"); 
         zPos = PlayerPrefs.GetFloat("PlZPos");
-        // PlayerPrefs.GetFloat("PlXRot", xRot);
-        // PlayerPrefs.GetFloat("PlYRot", yRot);
-        // PlayerPrefs.GetFloat("PlZRot", zRot);
         thePlayer.transform.position = new Vector3(xPos, yPos, zPos);
-        // thePlayer.transform.rotation = new Quaternion(xRot, yRot, zRot);
     }
 
     // Update is called once per frame
     void Update(){
         xPos = thePlayer.transform.position.x;
         yPos = thePlayer.transform.position.y;
-        zPos = thePlayer.transform.position.z;
-
-        // xRot = thePlayer.transform.rotation.x;
-        // yRot = thePlayer.transform.rotation.y;
-        // zRot = thePlayer.transform.rotation.z;
-        
+        zPos = thePlayer.transform.position.z;        
     }
 
     void OnTriggerEnter(Collider collider){
@@ -39,8 +30,6 @@ public class SaveStatePlayerPrefs : MonoBehaviour
         PlayerPrefs.SetFloat("PlYPos", yPos);
         PlayerPrefs.SetFloat("PlZPos", zPos);
 
-        // PlayerPrefs.SetFloat("PlXRot", xRot);
-        // PlayerPrefs.SetFloat("PlYRot", yRot);
-        // PlayerPrefs.SetFloat("PlZRot", zRot);
+        print("Saved");
     }
 }
